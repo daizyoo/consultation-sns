f6301e57e135ccfefb8684a0d4776d76193aa733
daizyoo <145541736+dY-8889@users.noreply.github.com>
2024-04-17 11:58:14 +0900

    commit

--- app/script/user/create.js
+++ app/script/user/create.js
@@ -1,14 +1,27 @@
 const create = () => {
-    const name = document.getElementById('name').value;
-    const id = document.getElementById('id').value;
-    const password = document.getElementById('password').value;
+  const name = document.getElementById("name").value;
+  const id = document.getElementById("id").value;
+  const password = document.getElementById("password").value;
 
-    if (name === '') { document.getElementById('message').textContent = '名前を入力してください'; return; }
-    if (id === '') { document.getElementById('message').textContent = 'idを入力してください'; return; }
-    if (password === '') { document.getElementById('message').textContent = 'パスワードを入力してください'; return; }
+  if (name === "") {
+    document.getElementById("message").textContent = "名前を入力してください";
+    return;
+  }
+  if (id === "") {
+    document.getElementById("message").textContent = "idを入力してください";
+    return;
+  }
+  if (password === "") {
+    document.getElementById("message").textContent =
+      "パスワードを入力してください";
+    return;
+  }
 
-    fetch("/api/user/create", postHeader()).then(res => res.json()).then(res => {
-        if (res.status == true) window.location.href = '/';
-        else document.getElementById('message').textContent = res.data;
-    })
-}
+  fetch("/api/user/create", postHeader())
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.status == true) window.location.href = "/";
+      else document.getElementById("message").textContent = res.data;
+    });
+};
+
